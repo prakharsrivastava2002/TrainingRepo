43b5c2796deb567a1d310112af82f9305d676cd7
Prakhar <prakhar25jan@gmail.com>
2023-02-14 15:10:26 +0530

    Second Commit

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
 class demo {
-	public static voind main(Strings args[]){System.out.println("this is my first java program");
+	public static void main(Strings args[]){System.out.println("this is my first java program");
 	}
 }
